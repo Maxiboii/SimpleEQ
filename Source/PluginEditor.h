b85e3ec043622f1deea2b2a9ca1aba7290921e2f
Maxiboii <maksimchichkan90@gmail.com>
2022-03-29 22:55:17 +0200

    Implemented Analyzer Enablement Button

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -238,6 +238,11 @@ juce::Timer
     
     void paint(juce::Graphics& g) override;
     void resized() override;
+    
+    void toggleAnalysisEnablement(bool enabled)
+    {
+        shouldShowFFTAnalysis = enabled;
+    }
 private:
     SimpleEQAudioProcessor& audioProcessor;
     juce::Atomic<bool> parametersChanged { false };
@@ -253,6 +258,8 @@ private:
     juce::Rectangle<int> getAnalysisArea();
     
     PathProducer leftPathProducer, rightPathProducer;
+    
+    bool shouldShowFFTAnalysis = true;
 };
 
 //==============================================================================
